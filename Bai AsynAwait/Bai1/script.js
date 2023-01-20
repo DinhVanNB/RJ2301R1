34bb5c1da2984dd518b309082b00031aeaf2323a
DinhVanNB <phuongvan2430@gmail.com>
2023-01-20 21:29:33 +0700

    add BT

--- Bai AsynAwait/Bai1/script.js
+++ Bai AsynAwait/Bai1/script.js
@@ -7,29 +7,32 @@ let keyAPI = 'OW32MmAEHypvCkn0H2aMByrOB5vIvRX6';
 async function getData(keyWord, keyAPI) {
     try {
         let data = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${keyWord}&api_key=${keyAPI}`);
-        imageData = data.data.data
+        imageData = data.data.data;
     } catch (e) {
         window.confirm(e);
     }
 }
-$('formInput').addEventListener('reset', () => $('root').innerHTML = '');
+$('formInput').addEventListener('reset',()=>$('root').innerHTML='');
 $('formInput').addEventListener('submit', (e) => onSubmitForm(e));
 
 async function onSubmitForm(e) {
     e.preventDefault();
     keyWord = $('inp').value;
     await getData(keyWord, keyAPI);
-    try {
+    try{
         displayImage(imageData);
-    } catch (e) {
-        window.confirm('Không tìm thấy kết quả ' + e);
+    }
+    catch(e){
+        window.confirm('Không tìm thấy kết quả ' +e);
     }
 }
 
 displayImage = (imageData) => {
-    let dataUrl = imageData.map(img => img.images.fixed_height.url)
-
-    $('root').innerHTML += `<img class="img-thumbnail mx-1 mb-2"
-        // src="${dataUrl[0]}" >`
-
+   let dataUrl = imageData.map(img => 
+        img.images.fixed_height.url
+    )
+    for(let i =0; i<10;i++){
+        $('root').innerHTML += `<img class="img-thumbnail mx-1 mb-2"
+        // src="${dataUrl[i]}" >`
+    }
 }
