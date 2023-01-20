5015d84fd8d8e8e128c054bce388222d346a1b88
DinhVanNB <phuongvan2430@gmail.com>
2023-01-20 19:11:42 +0700

    add BT

---
+++ Bai AsynAwait/Bai1/script.js
@@ -0,0 +1,35 @@
+let imageData,
+    keyWord;
+let $ = document
+    .getElementById
+    .bind(document);
+let keyAPI = 'OW32MmAEHypvCkn0H2aMByrOB5vIvRX6';
+async function getData(keyWord, keyAPI) {
+    try {
+        let data = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${keyWord}&api_key=${keyAPI}`);
+        imageData = data.data.data
+    } catch (e) {
+        window.confirm(e);
+    }
+}
+$('formInput').addEventListener('reset', () => $('root').innerHTML = '');
+$('formInput').addEventListener('submit', (e) => onSubmitForm(e));
+
+async function onSubmitForm(e) {
+    e.preventDefault();
+    keyWord = $('inp').value;
+    await getData(keyWord, keyAPI);
+    try {
+        displayImage(imageData);
+    } catch (e) {
+        window.confirm('Không tìm thấy kết quả ' + e);
+    }
+}
+
+displayImage = (imageData) => {
+    let dataUrl = imageData.map(img => img.images.fixed_height.url)
+
+    $('root').innerHTML += `<img class="img-thumbnail mx-1 mb-2"
+        // src="${dataUrl[0]}" >`
+
+}
